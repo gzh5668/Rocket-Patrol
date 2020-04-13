fa0d6e8e3b6f22531f8c8a8cd4b02230c374805f
Zihan Guo <zguo24@ucsc.edu>
2020-04-13 03:27:43 -0700

    Collision Checking Done

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -34,8 +34,30 @@ class Play extends Phaser.Scene {
     update() {
         this.starfield.tilePositionX -= 4;
         this.p1Rocket.update();
-        this.ship01.update(); // update spaceships
+        // update spaceships
+        this.ship01.update();
         this.ship02.update();
         this.ship03.update();
+        // check collisions
+        if(this.checkCollision(this.p1Rocket, this.ship03)) {
+            console.log('kaboom ship 03');
+        }
+        if (this.checkCollision(this.p1Rocket, this.ship02)) {
+            console.log('kaboom ship 02');
+        }
+        if (this.checkCollision(this.p1Rocket, this.ship01)) {
+            console.log('kaboom ship 01');
+        }
+    }
+    checkCollision(rocket, ship) {
+        // simple AABB checking
+        if (rocket.x < ship.x + ship.width && 
+            rocket.x + rocket.width > ship.x && 
+            rocket.y < ship.y + ship.height &&
+            rocket.height + rocket.y > ship. y) {
+                return true;
+        } else {
+            return false;
+        }
     }
 }
