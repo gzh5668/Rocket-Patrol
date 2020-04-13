b6285dbc675b6f594d016cafc05555c75b9adcdb
Zihan Guo <zguo24@ucsc.edu>
2020-04-13 01:17:25 -0700

    2ed test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 //parameters
 let config = {
-    type: Phaser.CANVAS,
+    type: Phaser.AUTO,
     width: 640,
-    height: 480
+    height: 480,
+    scene: [ Menu, Play ]
 }
 let game = new Phaser.Game(config);
